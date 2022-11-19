b3a52267c0333f37fb0d79ed988dd918d1c22a96
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2022-11-19 00:13:40 -0500

    finalize timer button function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Breathe Button
+
+let breatheButton = document.querySelector(".breathe-button");
+
 function countdown() {
     let seconds = 60;
     let mins = 1;
@@ -8,27 +12,19 @@ function countdown() {
         counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
         if( seconds > 0 ) {
             setTimeout(tick, 1000);
-
         }
     }
     tick();
+    breatheButton.classList.add('breathe-button-active');
+    breatheButton.classList.remove('breathe-button');
+    setTimeout(() => {
+        breatheButton.classList.add('breathe-button');
+    }, 60000);
+    setTimeout(() => {
+        breatheButton.classList.remove('breathe-button-active');
+    }, 60000);
+    setTimeout(() => {
+        document.getElementById("audio").play() = '#659d25e2';
+    }, 60000);
 }
-
-function breatheButtonColorChange() {
-    breatheButton.style.backgroundColor = 'rgba(255, 166, 0, 0.758)';
-    setTimeout(breatheButtonOriginal, 60000);
-    setTimeout(playAudio, 60000);
-}
-
-function breatheButtonOriginal() {
-    breatheButton.style.backgroundColor = '#659d25e2';
-}
-
-function playAudio() {
-    document.getElementById("audio").play();
-}
-
-
-let breatheButton = document.querySelector(".breathe-button");
 breatheButton.addEventListener('click', countdown);
-breatheButton.addEventListener('click', breatheButtonColorChange);
